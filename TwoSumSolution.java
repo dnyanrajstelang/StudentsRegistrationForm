4e7030fc6745db1291a912a1f917e703aad5b826
dnyanrajstelang <dnyanrajstelang1997@gmail.com>
2023-11-20 19:37:36 +0530

    two sum solution

---
+++ TwoSumSolution.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSumSolution {
+    public static void main(String[] args) {
+        int [] nums = {1,2,7,5,4};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+
+    private static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
+        for(int i = 0 ; i < nums.length ; i++){
+            if(map.containsKey(target - nums[i])){
+                result[0] = i;
+                result[1] = map.get(target - nums[i]);
+                return result;
+            }else{
+                map.put(nums[i], i);
+            }
+        }
+        return result;
+    }
+}
